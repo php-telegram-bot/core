25ec8cf3987c45518d1963acd34990343d821e17
LONGMAN <akalongman@gmail.com>
2015-06-30 17:13:43 +0400

    Fix namespace bug

--- src/Entities/Chat.php
+++ src/Entities/Chat.php
@@ -1,13 +1,13 @@
 <?php
 /*
- * This file is part of the TelegramApi package.
+ * This file is part of the TelegramBot package.
  *
  * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace Longman\TelegramApi\Entities;
+namespace Longman\TelegramBot\Entities;
 
 
 class Chat
