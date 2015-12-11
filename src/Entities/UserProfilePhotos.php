c02657d5fbe22701b9568e98a1ab0a1953f16a95
Peter Notenboom <peter@digiter.nl>
2015-12-11 10:37:31 +0100

    Fix for /whoami with a user who doesn't have a profile picture
    Extra space in bracket

--- src/Entities/UserProfilePhotos.php
+++ src/Entities/UserProfilePhotos.php
@@ -22,11 +22,11 @@ class UserProfilePhotos extends Entity
     {
 
         $this->total_count = isset($data['total_count']) ? $data['total_count'] : null;
-        if ($this->total_count === null && !is_numeric($this->total_count) ) {
+        if ($this->total_count === null && !is_numeric($this->total_count)) {
             throw new TelegramException('total_count is empty!');
         }
         $this->photos = isset($data['photos']) ? $data['photos'] : null;
-        if ($this->photos === null || !is_array($data['photos']) ) {
+        if ($this->photos === null || !is_array($data['photos'])) {
             throw new TelegramException('photos is empty!');
         }
 
