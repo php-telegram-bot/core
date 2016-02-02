f7dd3d80f44912b7327217974aafb9cefe49ec20
Armando Lüscher <armando@noplanman.ch>
2016-02-02 10:47:16 +0100

    Initial code cleanup of `TelegramException.php`. No functional changes, only modifications of documentation and code layout.

--- src/Exception/TelegramException.php
+++ src/Exception/TelegramException.php
@@ -1,17 +1,26 @@
 <?php
-/*
+/**
  * This file is part of the TelegramBot package.
  *
  * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
-*/
+ */
+
 namespace Longman\TelegramBot\Exception;
 
+/**
+ * Main exception class used for exception handling
+ */
 class TelegramException extends \Exception
 {
-
+    /**
+     * Exception constructor
+     *
+     * @param string $message Error message
+     * @param integer $code   Error code
+     */
     public function __construct($message, $code = 0)
     {
         parent::__construct($message, $code);
@@ -19,9 +28,8 @@ class TelegramException extends \Exception
         $path = 'TelegramException.log';
         $status = file_put_contents(
             $path,
-            date('Y-m-d H:i:s', time()) .' '. self::__toString() . "\n",
+            date('Y-m-d H:i:s', time()) . ' ' . self::__toString() . "\n",
             FILE_APPEND
         );
-
     }
 }
