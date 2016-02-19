168e73382f7cac0ca549d1bc76e43847eb4b2999
Armando Lüscher <armando@noplanman.ch>
2016-02-19 18:01:14 +0100

    Add methods to determine command type.

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -273,4 +273,34 @@ abstract class Command
     {
         return $this->enabled;
     }
+
+    /**
+     * If this is a SystemCommand
+     *
+     * @return bool
+     */
+    public function isSystemCommand()
+    {
+        return ($this instanceof SystemCommand);
+    }
+
+    /**
+     * If this is an AdminCommand
+     *
+     * @return bool
+     */
+    public function isAdminCommand()
+    {
+        return ($this instanceof AdminCommand);
+    }
+
+    /**
+     * If this is a UserCommand
+     *
+     * @return bool
+     */
+    public function isUserCommand()
+    {
+        return ($this instanceof UserCommand);
+    }
 }
