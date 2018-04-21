404efd8e9372555a11b2e927b9177d94817b99de
Avtandil Kikabidze <akalongman@gmail.com>
2018-04-21 17:52:49 +0400

    Create http kernel class

---
+++ src/Http/Kernel.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of the TelegramBot package.
+ *
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Longman\TelegramBot\Http;
+
+use Longman\TelegramBot\Entities\Update;
+use Longman\TelegramBot\Telegram;
+
+class Kernel
+{
+    /**
+     * The application implementation.
+     *
+     * @var \Longman\TelegramBot\Telegram
+     */
+    protected $app;
+
+    /**
+     * Create a new HTTP kernel instance.
+     *
+     * @param  \Longman\TelegramBot\Telegram $app
+     *
+     * @return void
+     */
+    public function __construct(Telegram $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Handle an incoming HTTP request.
+     *
+     * @param  \Longman\TelegramBot\Request2 $request
+     *
+     * @return \Longman\TelegramBot\Response
+     *
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     */
+    public function handle(Request $request)
+    {
+        $params = $request->json();
+
+        $update = new Update($params->all(), $this->app->getBotUsername());
+
+        if ($response = $this->app->processUpdate($update)) {
+            return $response->isOk();
+        }
+
+        return $response;
+    }
+}
