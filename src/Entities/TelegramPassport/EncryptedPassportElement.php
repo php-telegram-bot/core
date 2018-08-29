ff8d0648e9412f9a03fe38a923074e90765c84af
Armando Lüscher <armando@noplanman.ch>
2018-08-29 23:04:30 +0200

    Bot API 4.1, add new fields and error types for Telegram Passport

--- src/Entities/TelegramPassport/EncryptedPassportElement.php
+++ src/Entities/TelegramPassport/EncryptedPassportElement.php
@@ -25,8 +25,8 @@ use Longman\TelegramBot\Entities\Entity;
  * @method string       getEmail()       Optional. User's verified email address, available only for “email” type
  * @method PassportFile getFrontSide()   Optional. Encrypted file with the front side of the document, provided by the user. Available for “passport”, “driver_license”, “identity_card” and “internal_passport”. The file can be decrypted and verified using the accompanying EncryptedCredentials.
  * @method PassportFile getReverseSide() Optional. Encrypted file with the reverse side of the document, provided by the user. Available for “driver_license” and “identity_card”. The file can be decrypted and verified using the accompanying EncryptedCredentials.
- * @method PassportFile getSelfie()      Optional. Encrypted file with the selfie of the user holding a document, provided by the user;
- *                                       available for “passport”, “driver_license”, “identity_card” and “internal_passport”. The file can be decrypted and verified using the accompanying EncryptedCredentials.
+ * @method PassportFile getSelfie()      Optional. Encrypted file with the selfie of the user holding a document, provided by the user; available for “passport”, “driver_license”, “identity_card” and “internal_passport”. The file can be decrypted and verified using the accompanying EncryptedCredentials.
+ * @method string       getHash()        Base64-encoded element hash for using in PassportElementErrorUnspecified
  **/
 class EncryptedPassportElement extends Entity
 {
@@ -40,11 +40,12 @@ class EncryptedPassportElement extends Entity
             'front_side'   => PassportFile::class,
             'reverse_side' => PassportFile::class,
             'selfie'       => PassportFile::class,
+            'translation'  => PassportFile::class,
         ];
     }
 
     /**
-     * Array with information about documents and other Telegram Passport elements that was shared with the bot
+     * Optional. Array of encrypted files with documents provided by the user, available for “utility_bill”, “bank_statement”, “rental_agreement”, “passport_registration” and “temporary_registration” types. Files can be decrypted and verified using the accompanying EncryptedCredentials.
      *
      * This method overrides the default getFiles method
      * and returns a nice array of PassportFile objects.
@@ -54,6 +55,21 @@ class EncryptedPassportElement extends Entity
     public function getFiles()
     {
         $pretty_array = $this->makePrettyObjectArray(PassportFile::class, 'files');
+
+        return empty($pretty_array) ? null : $pretty_array;
+    }
+
+    /**
+     * Optional. Array of encrypted files with translated versions of documents provided by the user. Available if requested for “passport”, “driver_license”, “identity_card”, “internal_passport”, “utility_bill”, “bank_statement”, “rental_agreement”, “passport_registration” and “temporary_registration” types. Files can be decrypted and verified using the accompanying EncryptedCredentials.
+     *
+     * This method overrides the default getTranslation method
+     * and returns a nice array of PassportFile objects.
+     *
+     * @return null|PassportFile[]
+     */
+    public function getTranslation()
+    {
+        $pretty_array = $this->makePrettyObjectArray(PassportFile::class, 'translation');
 
         return empty($pretty_array) ? null : $pretty_array;
     }
