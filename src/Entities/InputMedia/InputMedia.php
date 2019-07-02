06523c6c6f11ff42548f330f6fe8640bc8985044
Armando Lüscher <armando@noplanman.ch>
2019-07-02 17:58:56 +0000

    Abstract InputMedia methods weren't being propagated via method annotations.

--- src/Entities/InputMedia/InputMedia.php
+++ src/Entities/InputMedia/InputMedia.php
@@ -4,20 +4,5 @@ namespace Longman\TelegramBot\Entities\InputMedia;
 
 interface InputMedia
 {
-    /**
-     * @return string Type of the result.
-     */
-    public function getType();
 
-    /**
-     * @return string File to send.
-     */
-    public function getMedia();
-
-    /**
-     * @param string $media File to send.
-     *
-     * @return string
-     */
-    public function setMedia($media);
 }
