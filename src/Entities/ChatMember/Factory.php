62c38a247be746cda83076024355e450bc650242
Tii <mail@tii.one>
2021-06-26 01:51:50 +0200

    Filled the ChatMember Factory

--- src/Entities/ChatMember/Factory.php
+++ src/Entities/ChatMember/Factory.php
@@ -5,11 +5,28 @@ namespace Longman\TelegramBot\Entities\ChatMember;
 
 
 use Longman\TelegramBot\Entities\Entity;
+use Longman\TelegramBot\Exception\TelegramException;
 
 class Factory extends \Longman\TelegramBot\Entities\Factory
 {
     public function make(array $data, string $bot_username): Entity
     {
-        var_dump($data);
+        $status = $data['status'];
+        switch ($status) {
+            case 'creator':
+                return new ChatMemberOwner($data, $bot_username);
+            case 'administrator':
+                return new ChatMemberAdministrator($data, $bot_username);
+            case 'member':
+                return new ChatMemberMember($data, $bot_username);
+            case 'restricted':
+                return new ChatMemberRestricted($data, $bot_username);
+            case 'left':
+                return new ChatMemberLeft($data, $bot_username);
+            case 'kicked':
+                return new ChatMemberBanned($data, $bot_username);
+        }
+
+        throw new TelegramException('Unexpected ChatMember type');
     }
 }
