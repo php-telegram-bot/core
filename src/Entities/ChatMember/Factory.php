ee3a44348312c3c9e81ed20cada762665e1132b6
Tii <mail@tii.one>
2021-06-26 10:00:06 +0200

    Cleaned up ChatMember\Factory code

--- src/Entities/ChatMember/Factory.php
+++ src/Entities/ChatMember/Factory.php
@@ -11,21 +11,24 @@ class Factory extends \Longman\TelegramBot\Entities\Factory
 {
     public function make(array $data, string $bot_username): Entity
     {
-        switch ($data['status'] ?? '') {
-            case 'creator':
-                return new ChatMemberOwner($data, $bot_username);
-            case 'administrator':
-                return new ChatMemberAdministrator($data, $bot_username);
-            case 'member':
-                return new ChatMemberMember($data, $bot_username);
-            case 'restricted':
-                return new ChatMemberRestricted($data, $bot_username);
-            case 'left':
-                return new ChatMemberLeft($data, $bot_username);
-            case 'kicked':
-                return new ChatMemberBanned($data, $bot_username);
+        if (! isset($data['status'])) {
+            throw new TelegramException('Missing ChatMember status');
         }
 
-        throw new TelegramException('Unexpected ChatMember type');
+        $type = [
+            'creator'       => ChatMemberOwner::class,
+            'administrator' => ChatMemberAdministrator::class,
+            'member'        => ChatMemberMember::class,
+            'restricted'    => ChatMemberRestricted::class,
+            'left'          => ChatMemberLeft::class,
+            'kicked'        => ChatMemberBanned::class,
+        ];
+
+        if (! isset($type[$data['status']])) {
+            throw new TelegramException('Unexpected ChatMember status');
+        }
+
+        $class = $type[$data['status']];
+        return new $class($data, $bot_username);
     }
 }
