736269baa9911a3323c59631343303ddb31fe840
Tii <mail@tii.one>
2021-12-11 15:04:48 +0100

    Added the field name to the ChatInviteLink entity.

--- src/Entities/ChatInviteLink.php
+++ src/Entities/ChatInviteLink.php
@@ -23,6 +23,7 @@ namespace Longman\TelegramBot\Entities;
  * @method bool    getCreatesJoinRequest()      True, if users joining the chat via the link need to be approved by chat administrators
  * @method bool    getIsPrimary()               True, if the link is primary
  * @method bool    getIsRevoked()               True, if the link is revoked
+ * @method string  getName()                    Optional. Invite link name
  * @method int     getExpireDate()              Optional. Point in time (Unix timestamp) when the link will expire or has been expired
  * @method int     getMemberLimit()             Optional. Maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
  * @method int     getPendingJoinRequestCount() Optional. Number of pending join requests created using this link
