9c58e50479bff4ca6f1d57aa1014b49478f095cb
Tii <mail@tii.one>
2021-12-11 14:51:22 +0100

    Added the fields creates_join_request and pending_join_request_count to the ChatInviteLink entity.

--- src/Entities/ChatInviteLink.php
+++ src/Entities/ChatInviteLink.php
@@ -18,12 +18,14 @@ namespace Longman\TelegramBot\Entities;
  *
  * @link https://core.telegram.org/bots/api#chatinvitelink
  *
- * @method string  GetInviteLink()       The invite link. If the link was created by another chat administrator, then the second part of the link will be replaced with “…”
- * @method User    getCreator()          Creator of the link
- * @method bool    getIsPrimary()        True, if the link is primary
- * @method bool    getIsRevoked()        True, if the link is revoked
- * @method int     getExpireDate()       Optional. Point in time (Unix timestamp) when the link will expire or has been expired
- * @method int     getMemberLimit()      Optional. Maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
+ * @method string  GetInviteLink()              The invite link. If the link was created by another chat administrator, then the second part of the link will be replaced with “…”
+ * @method User    getCreator()                 Creator of the link
+ * @method bool    getCreatesJoinRequest()      True, if users joining the chat via the link need to be approved by chat administrators
+ * @method bool    getIsPrimary()               True, if the link is primary
+ * @method bool    getIsRevoked()               True, if the link is revoked
+ * @method int     getExpireDate()              Optional. Point in time (Unix timestamp) when the link will expire or has been expired
+ * @method int     getMemberLimit()             Optional. Maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
+ * @method int     getPendingJoinRequestCount() Optional. Number of pending join requests created using this link
  */
 class ChatInviteLink extends Entity
 {
