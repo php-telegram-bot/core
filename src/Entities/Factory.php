fcfa8a30c2d8f3e30017e2228634d679b6453c95
Armando Lüscher <armando@noplanman.ch>
2022-04-24 23:12:54 +0200

    Remove PHP8+ `mixed` type hint

--- src/Entities/Factory.php
+++ src/Entities/Factory.php
@@ -6,12 +6,12 @@ abstract class Factory
 {
     abstract public static function make(array $data, string $bot_username): Entity;
 
-    public static function resolveEntityClass(string $class, mixed $property, string $bot_username = ''): Entity
+    public static function resolveEntityClass(string $class, $property, string $bot_username = ''): Entity
     {
         if (is_a($property, $class)) {
             return $property;
         }
-        
+
         if (is_subclass_of($class, Factory::class)) {
             return $class::make($property, $bot_username);
         }
