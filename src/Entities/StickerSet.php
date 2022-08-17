571c0b3d46d81ef6d92652b30558d91aee7d5b40
Mohsen Falakedin <104103618+OxMohsen@users.noreply.github.com>
2022-08-18 00:48:53 +0430

    add field sticker_type and remove contains_masks

--- src/Entities/StickerSet.php
+++ src/Entities/StickerSet.php
@@ -18,9 +18,9 @@ namespace Longman\TelegramBot\Entities;
  *
  * @method string    getName()          Sticker set name
  * @method string    getTitle()         Sticker set title
+ * @method string    getStickerType()   Type of stickers in the set, currently one of “regular”, “mask”, “custom_emoji”
  * @method bool      getIsAnimated()    True, if the sticker set contains animated stickers
  * @method bool      getIsVideo()       True, if the sticker set contains video stickers
- * @method bool      getContainsMasks() True, if the sticker set contains masks
  * @method Sticker[] getStickers()      List of all set stickers
  * @method PhotoSize getThumb()         Optional. Sticker set thumbnail in the .WEBP or .TGS format
  */
