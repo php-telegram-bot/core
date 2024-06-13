193e7b3d0c8f1dc01ddeab2d23600663028603ce
Tii <mail@tii.one>
2024-06-13 18:31:31 +0200

    Added additional sendPhoto example

--- examples/sendPhoto.php
+++ examples/sendPhoto.php
@@ -5,12 +5,22 @@ require 'bootstrap.php';
 $bot = new \PhpTelegramBot\Core\Telegram($_SERVER['BOT_TOKEN']);
 
 try {
+
     $bot->sendPhoto([
         'chat_id' => $_SERVER['RECIPIENT_CHAT_ID'],
         ...\PhpTelegramBot\Core\Entities\InputFile::attachFile('photo', __DIR__.'/files/example_photo.jpeg'),
     ]);
 
+    // or
+
+    //    $bot->sendPhoto([
+    //        'chat_id' => $_SERVER['RECIPIENT_CHAT_ID'],
+    //    ] + \PhpTelegramBot\Core\Entities\InputFile::attachFile('photo', __DIR__.'/files/example_photo.jpeg'));
+
     echo 'Photo sent.';
+
 } catch (\PhpTelegramBot\Core\Exceptions\TelegramException $e) {
+
     echo 'ERROR: '.$e->getMessage();
+
 }
